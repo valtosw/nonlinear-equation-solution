f9e472b1850725b8833df40f4f886ac4a8fb3025
valtosw <volodijbessarabov@knu.ua>
2025-09-19 23:34:27 +0300

    add SolverResult record

---
+++ NonlinearEquationSolution/Core/Entities/SolverResult.cs
@@ -0,0 +1,10 @@
+﻿namespace NonlinearEquationSolution.Core.Entities
+{
+    public record SolverResult(
+        string MethodName,
+        double Root,
+        int AposterioriIterations,
+        int AprioriIterations,
+        double Epsilon,
+        string Comments);
+}
