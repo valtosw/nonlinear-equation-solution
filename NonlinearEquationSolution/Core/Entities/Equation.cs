b144b265b76863f552dfaac508888a8d1bca35a1
valtosw <volodijbessarabov@knu.ua>
2025-09-19 23:33:58 +0300

    add Equation class implementing IEquation interface

---
+++ NonlinearEquationSolution/Core/Entities/Equation.cs
@@ -0,0 +1,15 @@
+﻿using NonlinearEquationSolution.Core.Interfaces;
+
+namespace NonlinearEquationSolution.Core.Entities
+{
+    public class Equation : IEquation
+    {
+        public string Definition => "x³ + 3x² - x - 3 = 0";
+
+        public Func<double, double> Function => x => Math.Pow(x, 3) + 3 * Math.Pow(x, 2) - x - 3;
+
+        public Func<double, double> Derivative => x => 3 * Math.Pow(x, 2) + 6 * x - 1;
+
+        public Func<double, double> SecondDerivative => x => 6 * x + 6;
+    }
+}
