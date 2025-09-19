f80f6b3fee1630a3ba42ba4247cb208536eb87e7
valtosw <volodijbessarabov@knu.ua>
2025-09-19 23:36:17 +0300

    add NewtonSolver class implementing IEquationSolver

---
+++ NonlinearEquationSolution/Infrastructure/Solvers/NewtonSolver.cs
@@ -0,0 +1,15 @@
+﻿using NonlinearEquationSolution.Core.Entities;
+using NonlinearEquationSolution.Core.Interfaces;
+
+namespace NonlinearEquationSolution.Infrastructure.Solvers
+{
+    public class NewtonSolver : IEquationSolver
+    {
+        public string MethodName => throw new NotImplementedException();
+
+        public SolverResult Solve(IEquation equation, ProblemDefinition problem, double epsilon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
