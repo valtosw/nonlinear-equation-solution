4250aae7f0cbebab0fbb687a31b5a0be8f48897f
valtosw <volodijbessarabov@knu.ua>
2025-09-19 23:35:59 +0300

    add IEquationSolver interface

---
+++ NonlinearEquationSolution/Core/Interfaces/IEquationSolver.cs
@@ -0,0 +1,10 @@
+﻿using NonlinearEquationSolution.Core.Entities;
+
+namespace NonlinearEquationSolution.Core.Interfaces
+{
+    public interface IEquationSolver
+    {
+        string MethodName { get; }
+        SolverResult Solve(IEquation equation, ProblemDefinition problem, double epsilon);
+    }
+}
