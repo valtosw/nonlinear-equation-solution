b0c91a54f9825741172aff46f2e587ad503d0b96
valtosw <volodijbessarabov@knu.ua>
2025-09-19 23:34:13 +0300

    add ProblemDefinition record

---
+++ NonlinearEquationSolution/Core/Entities/ProblemDefinition.cs
@@ -0,0 +1,9 @@
+﻿namespace NonlinearEquationSolution.Core.Entities
+{
+    public record ProblemDefinition(
+        string Description,
+        double A,
+        double B,
+        double RelaxationInitialGuess = double.NaN,
+        double NewtonInitialGuess = double.NaN);
+}
