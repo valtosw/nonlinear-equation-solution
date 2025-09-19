ba2e82bf2a54e8e6c4ea952bb94ef034321a79f4
valtosw <volodijbessarabov@knu.ua>
2025-09-19 23:35:38 +0300

    Add IEquation interface

---
+++ NonlinearEquationSolution/Core/Interfaces/IEquation.cs
@@ -0,0 +1,10 @@
+﻿namespace NonlinearEquationSolution.Core.Interfaces
+{
+    public interface IEquation
+    {
+        string Definition { get; }
+        Func<double, double> Function { get; }
+        Func<double, double> Derivative { get; }
+        Func<double, double> SecondDerivative { get; }
+    }
+}
