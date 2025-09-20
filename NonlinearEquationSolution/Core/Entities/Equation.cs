749446ecfe2194ef202ce5b5dc2184182294e9fd
valtosw <volodijbessarabov@knu.ua>
2025-09-20 13:18:32 +0300

    update equation notation

--- NonlinearEquationSolution/Core/Entities/Equation.cs
+++ NonlinearEquationSolution/Core/Entities/Equation.cs
@@ -4,7 +4,7 @@ namespace NonlinearEquationSolution.Core.Entities
 {
     public class Equation : IEquation
     {
-        public string Definition => "x³ + 3x² - x - 3 = 0";
+        public string Definition => "x^3 + 3x^2 - x - 3 = 0";
 
         public Func<double, double> Function => x => Math.Pow(x, 3) + 3 * Math.Pow(x, 2) - x - 3;
 
