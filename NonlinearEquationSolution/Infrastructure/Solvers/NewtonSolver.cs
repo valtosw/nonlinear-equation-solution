0b01d93112bffcce6960d20863e92cf530c1dacd
valtosw <volodijbessarabov@knu.ua>
2025-09-21 11:34:21 +0300

    enhance convergence checks in NewtonSolver

--- NonlinearEquationSolution/Infrastructure/Solvers/NewtonSolver.cs
+++ NonlinearEquationSolution/Infrastructure/Solvers/NewtonSolver.cs
@@ -11,8 +11,7 @@ namespace NonlinearEquationSolution.Infrastructure.Solvers
         public SolverResult Solve(IEquation equation, ProblemDefinition problem, double epsilon)
         {
             double intialGuess = problem.NewtonInitialGuess;
-            // string convergenceMessage = CheckConvergenceConditions(equation, intialGuess);
-            string convergenceMessage = "Convergence conditions not checked"; // TODO: Implement this method
+            string convergenceMessage = CheckConvergenceConditions(equation, problem);
             double xPrev = intialGuess;
 
             int aprioriIterations = EstimateAprioriIterations(equation, problem, epsilon);
@@ -62,9 +61,56 @@ namespace NonlinearEquationSolution.Infrastructure.Solvers
             );
         }
 
-        private static string CheckConvergenceConditions(IEquation equation, double x0)
+        private static string CheckConvergenceConditions(IEquation equation, ProblemDefinition problem)
         {
-            throw new NotImplementedException();
+            var messages = new List<string>();
+
+            double x0 = problem.NewtonInitialGuess;
+
+            if (equation.Function(x0) * equation.SecondDerivative(x0) <= 0)
+            {
+                messages.Add("f(x0) * f''(x0) <= 0, convergence not guaranteed.");
+            }
+
+            (double A, double B) = (problem.A, problem.B);
+
+            var (m1, _) = GetMinMaxAbsForQuadratic(equation.Derivative, A, B);
+            var (_, M2) = GetMinMaxAbsForLinear(equation.SecondDerivative, A, B);
+
+            double initialError = B - A;
+
+            double q = (M2 * initialError) / (2 * m1);
+
+            if (q >= 1)
+            {
+                messages.Add("q >= 1, convergence not guaranteed.");
+            }
+
+            return messages.Count == 0 ? "Convergence conditions satisfied." : string.Join("; ", messages);
+        }
+
+        private static (double min, double max) GetMinMaxAbsForQuadratic(Func<double, double> quadFunc, double a, double b)
+        {
+            const double vertexX = -1.0;
+
+            var poinstTocheck = new List<double> { a, b };
+
+            if (a < vertexX && vertexX < b)
+            {
+                poinstTocheck.Add(vertexX);
+            }
+
+            var values = poinstTocheck.Select(p => Math.Abs(quadFunc(p)));
+
+            return (values.Min(), values.Max());
+        }
+
+        private static (double min, double max) GetMinMaxAbsForLinear(Func<double, double> linearFunc, double a, double b)
+        {
+            double valA = Math.Abs(linearFunc(a));
+            double valB = Math.Abs(linearFunc(b));
+
+            return (Math.Min(valA, valB), Math.Max(valA, valB));
         }
 
         private static int EstimateAprioriIterations(IEquation equation, ProblemDefinition problem, double epsilon)
